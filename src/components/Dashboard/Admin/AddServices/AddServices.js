2efa021c34e01af0be33d9ea6a1c4cad9fe4be53
abuhorainhero <abuhorain2016@gmail.com>
2020-10-23 05:04:53 +0600

    first commit

---
+++ src/components/Dashboard/Admin/AddServices/AddServices.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Sidebar from '../../Sheard/Sidebar/Sidebar';
+
+const AddServices = () => {
+    return (
+        <section>
+            <div className="row">
+                <div className="col-md-2">
+                    <Sidebar />
+                </div>
+                <div className="col-md-10">
+                    
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AddServices;
